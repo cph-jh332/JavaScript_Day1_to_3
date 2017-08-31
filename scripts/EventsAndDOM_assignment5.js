3de3820b033b068457e8a7440e185de40aba79d4
Joachim Hansen <craciez@live.dk>
2017-08-31 09:09:23 +0200

    this is last

--- scripts/EventsAndDOM_assignment5.js
+++ scripts/EventsAndDOM_assignment5.js
@@ -1,41 +1,45 @@
 var tableHTML = document.getElementById("tableContainer");
 
 var cars = [
-    { id: 1, year: 1997, make: 'Ford', model: 'E350', price: 3000 },
-    { id: 2, year: 1999, make: 'Chevy', model: 'Venture', price: 4900 },
-    { id: 3, year: 2000, make: 'Chevy', model: 'Venture', price: 5000 },
-    { id: 4, year: 1996, make: 'Jeep', model: 'Grand Cherokee', price: 4799 },
-    { id: 5, year: 2005, make: 'Volvo', model: 'V70', price: 44799 }
+    {id: 1, year: 1997, make: 'Ford', model: 'E350', price: 3000},
+    {id: 2, year: 1999, make: 'Chevy', model: 'Venture', price: 4900},
+    {id: 3, year: 2000, make: 'Chevy', model: 'Venture', price: 5000},
+    {id: 4, year: 1996, make: 'Jeep', model: 'Grand Cherokee', price: 4799},
+    {id: 5, year: 2005, make: 'Volvo', model: 'V70', price: 44799}
 ];
 
-var table = function(array){
+var table = function (array) {
     var content = "<thead><tr>";
-    for(item in array[0]){
+    for (item in array[0]) {
         content += "<th>" + item + "</th>"
     }
     content += "</tr></thead>";
     var mapped = array.map(
-        function(items){
+        function (items) {
             var list = "<tr>";
-            for(key in items){
-                list += "<td>"+items[key]+"</td>"
+            for (key in items) {
+                list += "<td>" + items[key] + "</td>"
             }
             list += "</tr>";
             return list;
         }
     );
     var joined = mapped.join("");
-    return "<table>"+content+joined+"</table>";
+    return "<table>" + content + joined + "</table>";
 };
 
 tableHTML.innerHTML = table(cars);
 
-document.getElementById("lessThanButton").addEventListener("click", function(event){
-    var filteredArray = cars.filter(
-        function(car){
-            return car.price < document.getElementById("lessThanPrice").value;
-        }
-    );
-    tableHTML.innerHTML = table(filteredArray);
-    event.preventDefault();
+document.getElementById("lessThanButton").addEventListener("click", function (event) {
+    if (document.getElementById("lessThanPrice").value > 0) {
+        var filteredArray = cars.filter(
+            function (car) {
+                return car.price < document.getElementById("lessThanPrice").value;
+            }
+        );
+        tableHTML.innerHTML = table(filteredArray);
+        event.preventDefault();
+    } else {
+        event.preventDefault();
+    }
 });
